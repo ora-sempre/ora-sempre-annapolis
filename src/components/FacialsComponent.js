ee3002660c953ef757e5cba7554f4614bc8a3107
object-ions <moshikoatia@gmail.com>
2024-10-22 21:55:33 -0400

    Update component
    Co-authored-by: Object-ions moshikoatia@gmail.com

--- src/components/FacialsComponent.js
+++ src/components/FacialsComponent.js
@@ -5,6 +5,9 @@ import microderm from '../assets/website-images/microderm.jpg';
 import bluelight from '../assets/website-images/blue-light.jpg';
 import redlight from '../assets/website-images/red-light.jpg';
 import microneedle from '../assets/website-images/microneedle.jpg';
+import signature from '../assets/website-images/signature.jpeg';
+import dermaplane from '../assets/website-images/dermaplane.jpeg';
+
 import './FacialsComponent.scss';
 
 const FacialsComponent = () => {
@@ -15,32 +18,60 @@ const FacialsComponent = () => {
       link: '',
       imageUrl: microderm,
       title: 'Deep Cleaning with Diamond Microdermabrasion',
-      description: 'Our Diamond Microdermabrasion Facial is a gentle, non-invasive exfoliating treatment that encourages new cell growth and clears clogged pores. It helps reduce acne scars and balances skin tone.',
-      price: '$299',
+      subtitle: 'EXFOLIATING AND CLARIFYING',
+      description:
+        'Our Diamond Microdermabrasion Facial is a gentle, non-invasive exfoliating treatment that encourages new cell growth and clears clogged pores. It helps reduce acne scars and balances skin tone. By stimulating collagen production, it enhances your complexion, leaving your skin brighter and more youthful.',
+      price: '$225',
     },
     {
       id: 2,
       link: '',
       imageUrl: microneedle,
       title: 'Nanoneedling',
-      description: 'Our Nanoneedling Rejuvenation Facial reduces the appearance of scars, smooths fine lines and wrinkles. Enjoy improved skin elasticity and a smoother texture with this advanced treatment.',
-      price: '$359',
+      subtitle: 'SKIN REJUVENATION',
+      description:
+        'Our Nanoneedling Rejuvenation Facial reduces the appearance of scars, including acne scars, and smooths fine lines and wrinkles. It addresses enlarged pores, evens skin tone, and tackles hyperpigmentation and dark spots. Enjoy improved skin elasticity and a smoother texture with this advanced treatment.',
+      price: '$350',
     },
     {
       id: 3,
       link: '',
       imageUrl: bluelight,
       title: 'Deep Cleaning with Blue Light Therapy',
-      description: 'Unwind with our Deep Cleansing Facial, which includes a calming massage and precise extractions. This treatment deeply purifies your skin, while our exclusive mask enhances your facial experience.',
-      price: '$225',
+      subtitle: 'SOOTHING AND PURIFYING',
+      description:
+        'Unwind with our Deep Cleansing Facial, which includes a calming massage and precise extractions. This all-inclusive treatment deeply purifies your skin, while our exclusive mask enhances your facial experience and delivers even better results.',
+      price: '$149',
     },
     {
       id: 4,
       link: '',
       imageUrl: redlight,
       title: 'Anti-Aging with Red Light',
-      description: 'Our nonsurgical facelift is crafted to lift sagging skin and stimulate collagen production. This cutting-edge treatment reduces acne scars and improves blood circulation.',
+      subtitle: 'LIFTING AND FIRMING',
+      description:
+        'Our nonsurgical facelift is crafted to lift sagging skin and stimulate collagen production. This cutting-edge treatment reduces acne scars and improves blood circulation, giving your skin a healthier glow. Enjoy a youthful look without the recovery time of traditional surgery.',
       price: '$499',
+    },
+    {
+      id: 5,
+      link: '',
+      imageUrl: signature,
+      title: 'Signature Facial',
+      subtitle: 'CLEANING AND BOOSTING',
+      description:
+        'Our Signature Facial is a luxury treatment, customized to target your specific skin concerns. It revitalizes tired, aging skin, boosts collagen production, and encourages cellular repair for firmer, youthful skin. It reduces fine lines and wrinkles, enhancing skin health and texture.',
+      price: '$199',
+    },
+    {
+      id: 6,
+      link: '',
+      imageUrl: dermaplane,
+      title: 'Dermaplaning',
+      subtitle: 'REJUVINATING',
+      description:
+        'Our Dermaplaning Facial is a non-invasive treatment with no downtime, free from chemicals. It assists with skin texture, scarring, sun damage, and discoloration, unveiling a brighter, healthier complexion. Enjoy a rejuvenated experience with your newly revealed skin looking fresher than ever.',
+      price: '$120',
     },
   ];
 
