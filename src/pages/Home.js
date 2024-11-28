c438f64883f8af27bac0892d14dedb52dbffb3d6
object-ions <moshikoatia@gmail.com>
2024-11-28 12:02:30 -0500

    Add modual component, its presentation, embed in 'Home'.
    Co-Authored-By: Object-ions <moshikoatia@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero';
 import FacialsComponent from '../components/FacialsComponent';
+import PromoModal from '../components/PromoModal';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FacialsComponent />
+      <PromoModal />
     </div>
   );
 };
