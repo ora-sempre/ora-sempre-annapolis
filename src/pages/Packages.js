5c32a4a1c2f0c87c65cf3a1ddeae105ae0dd7636
object-ions <moshikoatia@gmail.com>
2024-12-23 13:48:44 -0500

    Add and update packages and prices.
    co-authored-by: object-ions <moshikoatia@gmail.com>

--- src/pages/Packages.js
+++ src/pages/Packages.js
@@ -1,38 +1,28 @@
 import "./Packages.scss";
 const packages = [
   {
-    title: "Silver",
-    features: [
-      "1 Customized Plan",
-      "4 In-Clinic Customized Facial Treatments",
-      "Skin Boosters / Enhancements Add-On Included",
-      "Skincare Included",
-      "Renelif Eye Included",
-    ],
+    title: "Diamond Plan",
+    price: "$8,500",
+  },
+  {
+    title: "Platinum Plan",
+    price: "$7,500",
+  },
+  {
+    title: "Gold Plan",
+    price: "$5,500",
+  },
+  {
+    title: "Silver Plan",
+    price: "$3,500",
+  },
+  {
+    title: "Bronze Plan",
     price: "$2,500",
   },
   {
-    title: "Gold",
-    features: [
-      "1 Customized Plan",
-      "8 In-Clinic Customized Facial Treatments",
-      "Skin Boosters / Enhancements Add-On Included",
-      "Skincare Included",
-      "Eneo Totale Medical Device Included",
-    ],
-    price: "$4,500",
-  },
-  {
-    title: "Platinum",
-    features: [
-      "1 Customized Plan",
-      "12 In-Clinic Customized Facial Treatments",
-      "4 In-Clinic Body Tightening Treatments",
-      "Skin Boosters / Enhancements Add-On Included",
-      "Skincare Included",
-      "Eneo Totale Medical Device Included",
-    ],
-    price: "$8,500",
+    title: "Pink Plan",
+    price: "$850",
   },
 ];
 const Packages = () => {
@@ -43,14 +33,8 @@ const Packages = () => {
         {packages.map((pkg, index) => (
           <div key={index} className="package-card">
             <h2 className="package-title">{pkg.title}</h2>
-            <ul className="package-features">
-              {pkg.features.map((feature, i) => (
-                <li key={i} className="package-feature">
-                  {feature}
-                </li>
-              ))}
-            </ul>
             <h3 className="package-price">{pkg.price}</h3>
+            <a href="https://affirm.orasempreannapolis.com">Shop with Affirm</a>
           </div>
         ))}
       </div>
